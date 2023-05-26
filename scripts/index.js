c6b1d03f2757b9302c4589af973f7248d566307f
DexinJ <dejin@ucsc.edu>
2023-05-26 13:50:42 -0700

    project 4 atage 3

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,23 @@ let initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+let content = document.querySelector(".content");
+let nameButton = content.querySelector(".profile__button_type_edit");
+let nameModal = document.querySelector(".modal");
+let closeButton = nameModal.querySelector(".modal__close");
+
+function displayModal(operation) {
+  if (operation) {
+    nameModal.classList.add("modal_opened");
+  } else {
+    nameModal.classList.remove("modal_opened");
+  }
+}
+nameButton.addEventListener("click", function () {
+  displayModal(true);
+});
+closeButton.addEventListener("click", function () {
+  displayModal(false);
+});
+console.log(nameButton);
