3437fa802827fbcea77ec199d42a5fa1b795ddb8
DexinJ <dejin@ucsc.edu>
2023-05-28 11:52:35 -0700

    sprint 4 final stage

--- scripts/index.js
+++ scripts/index.js
@@ -25,10 +25,39 @@ let initialCards = [
   },
 ];
 
-let content = document.querySelector(".content");
-let nameButton = content.querySelector(".profile__button_type_edit");
-let nameModal = document.querySelector(".modal");
-let closeButton = nameModal.querySelector(".modal__close");
+const nameButton = document.querySelector(".profile__button_type_edit");
+const nameModal = document.querySelector(".modal");
+const closeButton = nameModal.querySelector(".modal__close");
+const personName = document.querySelector(".profile__name");
+const personTitle = document.querySelector(".profile__title");
+const profileForm = document.querySelector(".modal__form");
+const nameInput = profileForm.querySelector("[name = 'name']");
+const titleInput = profileForm.querySelector("[name = 'title']");
+const cardTemplate = document.querySelector("#card").content;
+const cardGallery = document.querySelector(".gallery__cards");
+
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  personName.textContent = nameInput.value;
+  personTitle.textContent = titleInput.value;
+  nameInput.value = "";
+  titleInput.value = "";
+  renderPage();
+  displayModal(false);
+}
+
+function renderName() {
+  nameInput.placeholder = personName.textContent;
+  titleInput.placeholder = personTitle.textContent;
+}
+
+function getCardElement(data) {
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = data.link;
+  cardElement.querySelector(".card__image").alt = data.name;
+  cardElement.querySelector(".card__text").textContent = data.name;
+  return cardElement;
+}
 
 function displayModal(operation) {
   if (operation) {
@@ -37,10 +66,15 @@ function displayModal(operation) {
     nameModal.classList.remove("modal_opened");
   }
 }
+
+renderName();
 nameButton.addEventListener("click", function () {
   displayModal(true);
 });
 closeButton.addEventListener("click", function () {
   displayModal(false);
 });
-console.log(nameButton);
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+for (let i of initialCards) {
+  cardGallery.append(getCardElement(i));
+}
